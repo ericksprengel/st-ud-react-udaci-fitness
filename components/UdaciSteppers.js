2b2e0fd91a14809c5bb754bea59169d954909e98
Erick Massa Sprengel <erick.sprengel@gmail.com>
2019-01-28 08:49:54 -0200

    feat: implement the steppers

--- components/UdaciSteppers.js
+++ components/UdaciSteppers.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import {
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native'
+import {
+  FontAwesome,
+  Entypo,
+} from '@expo//vector-icons'
 
-class UdaciSteppers extends Component {
-  render() {
-    return (
-      <View>
-        <Text>UdaciSteppers</Text>
-      </View>
-    )
-  }
-}
+const UdaciSteppers = ({
+  value,
+  unit,
+  onDecrement,
+  onIncrement,
+  step,
+  max,
+}) => (
+  <View style={{flexDirection: 'row'}}>
+    <View>
+      <TouchableOpacity onPress={onDecrement}>
+        <FontAwesome name='minus' size={30} color='black' />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={onIncrement}>
+        <FontAwesome name='plus' size={30} color='black' />
+      </TouchableOpacity>
+    </View>
+    <View>
+      <Text>{value}</Text>
+      <Text>{unit}</Text>
+    </View>
+  </View>
+)
 
 export default UdaciSteppers
