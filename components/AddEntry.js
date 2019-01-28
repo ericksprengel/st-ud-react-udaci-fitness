288b88f8c01abc0bb634b8b7076fe0dbfae006c4
Erick Massa Sprengel <erick.sprengel@gmail.com>
2019-01-28 08:50:22 -0200

    feat: add already logged view

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react'
 import {
   SafeAreaView,
   View,
-  Text
+  Text,
+  TouchableOpacity
 } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helpers'
+import { Ionicons } from '@expo/vector-icons'
+import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
+import TextButton from './TextButton'
+
+const SubmitBtn = ({ onPress }) => (
+  <TouchableOpacity
+    onPress={onPress}
+  >
+    <Text>SUBMIT</Text>
+  </TouchableOpacity>
+)
 
 class AddEntry extends Component {
   state = {
@@ -48,8 +59,55 @@ class AddEntry extends Component {
     }))
   }
 
+  submit = () => {
+    const key = timeToString()
+    const entry = this.state
+
+    // Update Redux
+
+    this.setState(() => ({
+      run: 0,
+      bike: 0,
+      swim: 0,
+      sleep: 0,
+      eat: 0,
+    }))
+
+    // Navigate to home
+
+    // Save to 'DB'
+
+    // Clear local notification
+  }
+
+  reset = () => {
+    const key = timeToString()
+
+    // Update Redux
+
+    // Route to Home
+
+    // update "DB"
+  }
+
   render() {
     const metaInfo = getMetricMetaInfo()
+
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons
+            name='ios-happy'
+            size={100}
+          />
+          <Text>You already logged your information today</Text>
+          <TextButton onPress={this.reset}>
+            Reset
+          </TextButton>
+        </View>
+      )
+    }
+
     return (
       <SafeAreaView>
         <DateHeader date={(new Date()).toLocaleDateString()} />
@@ -80,6 +138,7 @@ class AddEntry extends Component {
             </View>
           )
         })}
+        <SubmitBtn onPress={this.submit} />
       </SafeAreaView>
     )
   }
