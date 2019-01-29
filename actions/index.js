c43c0bb67aa857f55cb0b0ebdad7d019ae2acbad
Erick Massa Sprengel <erick.sprengel@gmail.com>
2019-01-29 08:58:37 -0200

    feat: add redux actions

---
+++ actions/index.js
@@ -0,0 +1,17 @@
+export const RECEIVE_ENTRIES = 'RECEIVE_ENTRIES'
+export const ADD_ENTRY = 'ADD_ENTRY'
+
+const receiveEntries = (entries) => ({
+  type: RECEIVE_ENTRIES,
+  entries,
+})
+
+const addEntry = (entry) => ({
+  type: ADD_ENTRY,
+  entry,
+})
+
+export {
+  receiveEntries,
+  addEntry,
+}
